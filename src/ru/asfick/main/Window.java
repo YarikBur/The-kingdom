bb9a15bc4b351a580bd0bca2b341df2e40a854bf
Unknown <yarikbur@Mail.ru>
2018-01-25 21:05:13 +0300

    Add Keyboard Handler

--- src/ru/asfick/main/Window.java
+++ src/ru/asfick/main/Window.java
@@ -1,19 +1,47 @@
 package ru.asfick.main;
 
-import org.lwjgl.*;
-import org.lwjgl.glfw.*;
-import org.lwjgl.opengl.*;
-import org.lwjgl.system.*;
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.GLFW_FALSE;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
+import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
+import static org.lwjgl.glfw.GLFW.GLFW_TRUE;
+import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
+import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
+import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
+import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
+import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
+import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
+import static org.lwjgl.glfw.GLFW.glfwGetWindowSize;
+import static org.lwjgl.glfw.GLFW.glfwInit;
+import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
+import static org.lwjgl.glfw.GLFW.glfwPollEvents;
+import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowShouldClose;
+import static org.lwjgl.glfw.GLFW.glfwShowWindow;
+import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
+import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
+import static org.lwjgl.glfw.GLFW.glfwTerminate;
+import static org.lwjgl.glfw.GLFW.glfwWindowHint;
+import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+import java.nio.IntBuffer;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWKeyCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.system.MemoryStack;
 
 import ru.asfick.render.Render;
-
-import java.nio.*;
-
-import static org.lwjgl.glfw.Callbacks.*;
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryStack.*;
-import static org.lwjgl.system.MemoryUtil.*;
+import ru.asfick.utils.KeyboardHandler;
 
 /**
  * Класс, создающий окно
@@ -21,6 +49,8 @@ import static org.lwjgl.system.MemoryUtil.*;
  * @version 0.1
  */
 public class Window {
+	private GLFWKeyCallback keyCallback;
+	
 	private long window;
 	private long monitor;
 	private long share;
@@ -62,6 +92,7 @@ public class Window {
 		
 		System.out.println("Bie");
 		
+		keyCallback.close();
 		glfwFreeCallbacks(window);
 		glfwDestroyWindow(window);
 		
@@ -81,10 +112,11 @@ public class Window {
 		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 		
-		
 		window = glfwCreateWindow(width, height, title, monitor, share);
 		if(window == NULL) 
 			throw new RuntimeException("Failed to create the GLFW window");
+		
+		glfwSetKeyCallback(window, keyCallback = new KeyboardHandler());
 		
 		glfwSetKeyCallback(window, (window, key, scancode, action, mods) ->{
 			if(key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) glfwSetWindowShouldClose(window, true);
@@ -112,26 +144,19 @@ public class Window {
 	 * Бесконечный цикл (пока не закроется окно)
 	 */
 	private void loop() {
-		GL.createCapabilities();
-		
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
-		glOrtho(0, width, 0, height, 1, -1);
-		glMatrixMode(GL_VIEWPORT);
-		
-		glClearColor(0.8f, 0.2f, 0.2f, 1f);
+		rend.preRender(width, height);
 		
 		while(!glfwWindowShouldClose(window)) {  //отрисовка и расчеты
+			rend.updateBeforeDrawing();
+			
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 			
-			rend.updateBeforeDrawing();
 			rend.render(width, height);
-			rend.updateAfterDrawing();
 			
 			glfwSwapBuffers(window);
-			
 			glfwPollEvents();
 			
+			rend.updateAfterDrawing();
 		}
 	}
 }
--- src/ru/asfick/render/Render.java
+++ src/ru/asfick/render/Render.java
@@ -1,5 +1,16 @@
 package ru.asfick.render;
 
+import static org.lwjgl.opengl.GL11.GL_PROJECTION;
+import static org.lwjgl.opengl.GL11.GL_VIEWPORT;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glMatrixMode;
+import static org.lwjgl.opengl.GL11.glOrtho;
+import static org.lwjgl.glfw.GLFW.*;
+
+import org.lwjgl.opengl.GL;
+
+import ru.asfick.utils.KeyboardHandler;
 
 /**
  * Класс для прорисовки
@@ -15,6 +26,27 @@ public class Render {
 	}
 	
 	/**
+	 * Выполняет стандартную настройку перед рендером
+	 * @param width - ширина окна
+	 * @param height - высота окна
+	 */
+	public void preRender(int width, int height) {
+		GL.createCapabilities();
+		
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(0, width, 0, height, 1, -1);
+		glMatrixMode(GL_VIEWPORT);
+		
+		glClearColor(0.8f, 0.2f, 0.2f, 1f);
+	}
+	
+	private void testInputKeyboard() {
+		if(KeyboardHandler.isKeyDown(GLFW_KEY_W))
+			System.out.println("W Key Pressed");
+	}
+	
+	/**
 	 * Производит отрисовку изображения в окне
 	 * @param width - ширина окна
 	 * @param height - высота окна
@@ -27,6 +59,6 @@ public class Render {
 	 * Производит расчеты после прорисовки изображения
 	 */
 	public void updateAfterDrawing() {
-		
+		testInputKeyboard();
 	}
 }
